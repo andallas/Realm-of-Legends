991833e6daeebdf5820b32997a4496ddbafbb068
Justin <justin.35f@gmail.com>
2015-10-25 21:35:57 -0700

    Implemented basic mipmapping

--- src/renderEngine/TextureLoader.java
+++ src/renderEngine/TextureLoader.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
+import org.lwjgl.opengl.GL30;
 import org.newdawn.slick.opengl.Texture;
 
 public class TextureLoader
@@ -38,6 +40,9 @@ public class TextureLoader
 				break;
 			}
 			texture = org.newdawn.slick.opengl.TextureLoader.getTexture(format, new FileInputStream("res/Textures/" + fileName));
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -1);
 		}
 		catch (FileNotFoundException e)
 		{
